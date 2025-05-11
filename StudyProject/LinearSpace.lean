50270ae1e8f9a21c66da2a00c1663b4f86f5de7b
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-05-11 08:31:25 +0200

    +Basis

--- StudyProject/LinearSpace.lean
+++ StudyProject/LinearSpace.lean
@@ -13,142 +13,98 @@ class Field(elemType: Type)
   extends RingComOne elemType set sum mul where
 MultInv: ∀a:elemType, a∈set → ¬(a = zero) → (∃b:elemType, b∈set ∧ mul a b = one)
 
+-- list length
+def length{T:Type}: List T → ℕ
+| List.nil => 0
+| List.cons _ l => Nat.succ (length l)
+
+-- list prop
+def listProp{T:Type}: List T → (T → Prop) → Prop
+| List.nil, _         => 0=0
+| (List.cons x l), pr => (pr x) ∧ (listProp l pr)
+
 -- linear space
 class LinSp(TCoef TVec: Type)
            (fi: Set TCoef)
-           (sumC: TCoef → TCoef → TCoef)
-           (mulC: TCoef → TCoef → TCoef)
+           (sumF: TCoef → TCoef → TCoef)
+           (mulF: TCoef → TCoef → TCoef)
            (mulV: TCoef → TVec → TVec)
            (sumV: TVec → TVec → TVec)
-           extends Field TCoef fi sumC mulC
+           extends Field TCoef fi sumF mulF
            where
+  -- basic axioms
   VZero: TVec
-  VsumComm: ∀v1 v2: TVec, sumV v1 v2 = sumV v2 v1
-  VsumAssoc: ∀v1 v2 v3: TVec, sumV (sumV v1 v2) v3 = sumV v1 (sumV v2 v3)
-  VMulLin: ∀v1 v2: TVec, ∀c: TCoef, c∈fi → mulV c (sumV v1 v2) = sumV (mulV c v1) (mulV c v2)
-  VMulZero: ∀v: TVec, mulV zero v = VZero
-  VMulOne: ∀v: TVec, mulV one v = v
+  VSumComm: ∀v1 v2: TVec, sumV v1 v2 = sumV v2 v1
+  VSumAssoc: ∀v1 v2 v3: TVec, sumV (sumV v1 v2) v3 = sumV v1 (sumV v2 v3)
+  VMultLin: ∀v1 v2: TVec, ∀c: TCoef, c∈fi → mulV c (sumV v1 v2) = sumV (mulV c v1) (mulV c v2)
+  VMultZero: ∀v: TVec, mulV zero v = VZero
+  VMultOne: ∀v: TVec, mulV one v = v
+  VMultVZero: ∀v: TVec, sumV VZero v = v
 
--- element of tensor product of spaces
--- sum of pairs from A and fom B
-def VecTen(TVA: Type)(TVB:Type) := TVA → TVB
+-- linear combination
+def linCombImp{TCoef TVec: Type}
+           {fi: Set TCoef}
+           {sumF: TCoef → TCoef → TCoef}
+           {mulF: TCoef → TCoef → TCoef}
+           {mulV: TCoef → TVec → TVec}
+           {sumV: TVec → TVec → TVec}
+           (inst: LinSp TCoef TVec fi sumF mulF mulV sumV)
+           (lCoef: List TCoef)
+           (lVec: List TVec): TVec :=
+match lCoef, lVec with
+| List.nil, List.nil                 => inst.VZero
+| (List.cons c lc), (List.cons v lv) => sumV (mulV c v) (linCombImp inst lc lv)
+| (List.cons _ _), List.nil          => inst.VZero
+| List.nil, (List.cons _ _)          => inst.VZero
 
--- multiply element of tensor product by scalar
-def VecTenMul {TCoef TVA TVB: Type}
-                   (_: TCoef → TVA → TVA)
-                   (mulVB: TCoef → TVB → TVB)
-                   (coef: TCoef)
-                   (input: VecTen TVA TVB):
-                   VecTen TVA TVB :=
-  fun va:TVA => mulVB coef (input va)
+-- linear combination
+def linComb{TCoef TVec: Type}
+           {fi: Set TCoef}
+           {sumF: TCoef → TCoef → TCoef}
+           {mulF: TCoef → TCoef → TCoef}
+           {mulV: TCoef → TVec → TVec}
+           {sumV: TVec → TVec → TVec}
+           (inst: LinSp TCoef TVec fi sumF mulF mulV sumV)
+           (lCoef: List TCoef)
+           (lVec: List TVec)
+           (_: length lCoef = length lVec)
+           (_: listProp lCoef (fun c:TCoef => c∈fi)):
+           TVec := linCombImp inst lCoef lVec
 
--- sum elements of tensor product
-def VecTenSum {TVA TVB: Type}
-            (_: TVA → TVA → TVA)
-            (sumVB: TVB → TVB → TVB)
-            (input1: VecTen TVA TVB)
-            (input2: VecTen TVA TVB):
-            VecTen TVA TVB :=
-  fun va:TVA => sumVB (input1 va) (input2 va)
+-- finite-dimension linear space
+class LinSpFD(TCoef TVec: Type)
+             (fi: Set TCoef)
+             (sumF: TCoef → TCoef → TCoef)
+             (mulF: TCoef → TCoef → TCoef)
+             (mulV: TCoef → TVec → TVec)
+             (sumV: TVec → TVec → TVec)
+             extends base:LinSp TCoef TVec fi sumF mulF mulV sumV
+             where
+  Basis: List TVec
+  ax1(aC: List TCoef)
+     (p1: length aC = length Basis)
+     (p2: listProp aC (fun c:TCoef => c∈fi)):
+        linComb base aC Basis p1 p2 = VZero →
+        listProp aC (fun c:TCoef => c=zero)
+  ax2(v:TVec):
+    ∃aC: List TCoef,
+    ∃p1:length aC = length Basis,
+    ∃p2:listProp aC (fun c:TCoef => c∈fi),
+    @linComb TCoef TVec fi sumF mulF mulV sumV base aC Basis p1 p2 = v
 
--- tensor product of two linear spaces
-def TensorProduct{TCoef TVA TVB: Type}
-                 {fi: Set TCoef}
-                 {sumC: TCoef → TCoef → TCoef}
-                 {mulC: TCoef → TCoef → TCoef}
-                 {mulVA: TCoef → TVA → TVA}
-                 {sumVA: TVA → TVA → TVA}
-                 {mulVB: TCoef → TVB → TVB}
-                 {sumVB: TVB → TVB → TVB}
-                 (_: LinSp TCoef TVA fi sumC mulC mulVA sumVA)
-                 (linSpB: LinSp TCoef TVB fi sumC mulC mulVB sumVB):
-                 (LinSp TCoef
-                        (VecTen TVA TVB)
-                        fi
-                        sumC
-                        mulC
-                        (VecTenMul mulVA mulVB)
-                        (VecTenSum sumVA sumVB)) :=
-  {
-    VZero := fun va:TVA => linSpB.VZero
-    VsumComm := by
-      simp [VecTen]
-      intro v1 v2
-      funext va
-      simp [VecTenSum]
-      apply linSpB.VsumComm
-    VsumAssoc := by
-      simp [VecTen]
-      intro v1 v2 v3
-      funext va
-      simp [VecTenSum]
-      apply linSpB.VsumAssoc
-    VMulLin := by
-      simp [VecTen]
-      intro v1 v2
-      intro c cIn
-      funext va
-      simp [VecTenSum, VecTenMul]
-      apply linSpB.VMulLin
-      apply cIn
-    VMulZero := by
-      simp [VecTen]
-      intro v1
-      funext va
-      simp [VecTenSum, VecTenMul]
-      apply linSpB.VMulZero
-    VMulOne := by
-      simp [VecTen]
-      intro v1
-      funext va
-      simp [VecTenSum, VecTenMul]
-      apply linSpB.VMulOne
-  }
---def Seqq:= ℕ → ℤ
-
---def mulV: ℤ → Seqq → Seqq
---| z, f => (fun n:ℕ => z*(f n))
-
---def sumV: Seqq → Seqq → Seqq
---| f1, f2 => (fun n:ℕ => (f1 n) + (f2 n))
-
---instance ls: LinSp ℤ Seqq set5 sum5 mul5 mulV sumV :=
---  {
---    VZero := (fun n:ℕ => 0)
---    VsumComm := by
---      intro v1 v2
---      simp [sumV]
---     funext n
---      omega
---    VsumAssoc := by
---      intro v1 v2 v3
---      simp [sumV]
---      funext n
---      omega
---    VMulLin := by
---      intro v1 v2
---      intro c cIn
---      simp [mulV, sumV]
---      funext n
---      generalize f: v1 n = A
---      generalize g: v2 n = B
- --     clear cIn n f g v1 v2
-  --    rw [Int.mul_comm]
-   --   rw [Int.mul_comm c A]
-    --  rw [Int.mul_comm c B]
---      apply Int.add_mul A B c
-  --  VMulZero := by
-    --  intro v
-      --funext n
---      simp [mulV]
-  --    rw [RingComOne.zero]
-    --  rw [Gal]
-      --simp
---    VMulOne := by
-  --    intro v
-    --  simp [mulV]
-      --funext n
---      rw [RingComOne.one]
-  --    rw [Gal]
-    --  simp
- -- }
+theorem th(TCoef TVec: Type)
+          (fi: Set TCoef)
+          (sumF: TCoef → TCoef → TCoef)
+          (mulF: TCoef → TCoef → TCoef)
+          (mulV: TCoef → TVec → TVec)
+          (sumV: TVec → TVec → TVec)
+          (linSp: LinSpFD TCoef TVec fi sumF mulF mulV sumV)
+          (aC1: List TCoef)
+          (aC2: List TCoef)
+          (pLen1: length aC1 = length linSp.Basis)
+          (pIn1: listProp aC1 (fun c:TCoef => c∈fi))
+          (pLen2: length aC2 = length linSp.Basis)
+          (pIn2: listProp aC2 (fun c:TCoef => c∈fi))
+          (eq: linComb linSp.base aC1 linSp.Basis pLen1 pIn1 = linComb linSp.base aC2 linSp.Basis pLen2 pIn2):
+          aC1 = aC2 := by
+  sorry
