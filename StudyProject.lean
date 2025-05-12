fcb75dd1532700129456d36cbcc59f8510d0d798
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-05-12 10:08:59 +0200

    refactoring

--- StudyProject.lean
+++ StudyProject.lean
@@ -2,10 +2,10 @@
 -- Import modules here that should be built as part of the library.
 import StudyProject.ClassExample1
 import StudyProject.Ring
+import StudyProject.RingTheorems
+import StudyProject.RingFunc
 import StudyProject.RingComOne
-import StudyProject.LinearSpace
+import StudyProject.Field
 import StudyProject.GaluaField
 import StudyProject.RingComOne_X_RingComOne
-import StudyProject.RingFunc
-import StudyProject.RingTheorems
-import StudyProject.Field
+import StudyProject.LinearSpace
