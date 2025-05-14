e8e72654bc67dfb9ef465999fca5a9817acc8a87
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-05-14 12:43:40 +0200

    a*0=0

--- StudyProject/RingTheorems.lean
+++ StudyProject/RingTheorems.lean
@@ -68,3 +68,46 @@ theorem ringTheorem2{t: Type}
   apply ring.sumDef
   apply aIn
   apply bIn
+
+-- a*0=t1
+-- a*(0+0)=a*(0+0)=t2
+-- a*(0+0)=a*0+a*0=t3
+-- a*0=a*0+a*0
+-- -a*0=t4
+-- a*0+(-a*0)=a*0+a*0+(-a*0)
+theorem multZero{T: Type}
+                {set: Set T}
+                {sum: T → T → T}
+                {mul: T → T → T}
+                (ring: Ring T set sum mul):
+  ∀a:T, a∈set → ((mul a ring.zero) = ring.zero) := by
+  intro a aIn
+
+  let t1 := mul a ring.zero
+  have t1In: t1∈set := by
+    apply ring.mulDef
+    apply aIn
+    apply ring.zeroEx
+  let t2 := mul a (sum ring.zero ring.zero)
+  let t3 := sum t1 t1
+  let ⟨t4, tmp⟩ := ring.sumRev t1 t1In
+  let ⟨t4In, t4Inv⟩ := tmp
+  clear tmp
+
+  have eq1: t1=t2 := by
+    simp [t1, t2, (ring.zeroProp ring.zero ring.zeroEx)]
+  have eq2: t2=t3 := by
+    simp [t2, t3, ring.multDistrLeft ring.zero ring.zero a ring.zeroEx ring.zeroEx aIn, t1]
+  have eq3: t1=t3 := by
+    apply Eq.trans eq1 eq2
+  have eq4: sum t1 t4 = sum t3 t4 := by
+    simp [eq3]
+
+  simp [t3] at eq4
+  rw [t4Inv] at eq4
+  rw [ring.sumAssoc t1 t1 t4 t1In t1In t4In] at eq4
+  rw [t4Inv] at eq4
+  rw [ring.zeroProp t1 t1In] at eq4
+  simp [t1] at eq4
+  rw [Eq.comm] at eq4
+  apply eq4
