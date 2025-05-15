d0e7798095dcaafa21cb567008fa595e76075221
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-05-15 13:18:03 +0200

    foo

--- StudyProject/ClassExample1.lean
+++ StudyProject/ClassExample1.lean
@@ -29,3 +29,20 @@ instance ex1Inst2: ex1Rev (fun x:ℤ => x*x) 4 :=
 
 theorem ex1Th2: ∃g:ℤ, g*g=4 := by
   apply ex1Th1 ex1Inst1
+
+structure Rational where
+  numerator: ℤ
+  denominator: ℤ
+
+  -- denominator must be positive
+  pos: denominator > 0
+  -- simpliest possible ratio
+  rat: ¬ (∃g:ℤ, g≠1 ∧ g≠(-1) ∧ g≠0 ∧ numerator%g=0 ∧ denominator%g=0)
+  -- zero unique
+  zero: numerator=0 → denominator=1
+
+class suuum(a b c: ℕ) where
+eq: a+b=c
+
+theorem foo[ef:suuum 1 1 3]:1+1=3:=by
+  apply ef.eq
