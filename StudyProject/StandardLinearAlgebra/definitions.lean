6f712fc0cdd3a59fb44d72464d9f97a173d80354
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-05-21 17:44:32 +0200

    refactoring

--- StudyProject/StandardLinearAlgebra/definitions.lean
+++ StudyProject/StandardLinearAlgebra/definitions.lean
@@ -1,20 +1,7 @@
 import Mathlib.Data.Int.Basic
-import Mathlib.Algebra.Ring.Basic
-import Mathlib.Algebra.Field.Basic
-import Mathlib.LinearAlgebra.Basis.VectorSpace
-import Mathlib.LinearAlgebra.Basis.Submodule
-import Mathlib.LinearAlgebra.Basis.Basic
-import Mathlib.Algebra.Module.Defs
-import Mathlib.Data.Fin.Basic
-import Mathlib.LinearAlgebra.DirectSum.Basis
-import Mathlib.LinearAlgebra.Matrix.Basis
-import Mathlib.LinearAlgebra.TensorProduct.Basic
-import Mathlib.Algebra.Module.Submodule.Basic
-import Mathlib.Algebra.Module.Submodule.Bilinear
-import Mathlib.LinearAlgebra.Basis.Basic
-import Mathlib.LinearAlgebra.BilinearForm.Basic
-import Mathlib.Data.Matrix.Kronecker
 
+-- Infinite sequence of integer numbers
 def SeqInf:Type := ℕ → ℤ
 
+-- Finite sequence of integer numbers
 def SeqFin(N: ℕ):Type := (Fin N) → ℤ
