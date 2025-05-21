9a2edcfbfe212fa90ec8627aad4f23ad7463e991
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-05-21 18:39:49 +0200

    comments

--- StudyProject/StandardLinearAlgebra/definitions.lean
+++ StudyProject/StandardLinearAlgebra/definitions.lean
@@ -1,7 +1,11 @@
 import Mathlib.Data.Int.Basic
 
 -- Infinite sequence of integer numbers
+-- Technically this is one type
 def SeqInf:Type := ℕ → ℤ
 
 -- Finite sequence of integer numbers
+-- Technically this is countable set of types
+-- (set of types isomorphic to natural numbers)
 def SeqFin(N: ℕ):Type := (Fin N) → ℤ
+#check SeqFin
