b12d7ae59ba639d4eb12409cd1bb1c3ab30eba99
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-05-21 08:03:59 +0200

    submodule

--- StudyProject/StandardLinearAlgebra/example.lean
+++ StudyProject/StandardLinearAlgebra/example.lean
@@ -9,6 +9,8 @@ import Mathlib.Data.Fin.Basic
 import Mathlib.LinearAlgebra.DirectSum.Basis
 import Mathlib.LinearAlgebra.Matrix.Basis
 import Mathlib.LinearAlgebra.TensorProduct.Basic
+import Mathlib.Algebra.Module.Submodule.Basic
+import Mathlib.Algebra.Module.Submodule.Bilinear
 
 def SeqInf:Type := ℕ → ℤ
 
@@ -408,3 +410,50 @@ def resT:=@instModule ℤ Int.instCommSemiring (SeqFin 2) (SeqFin 2) (GroupFinit
 #check resT.smul
 
 def simp:Type:=ℤ ⊗[ℤ] ℤ
+
+def sumZero(N: ℕ)(seq: SeqFin N) := (∑ i:Fin N, seq i) = 0
+
+def f(N: ℕ): Submodule ℤ (SeqFin N) :=
+{
+  carrier := {seq: SeqFin N | sumZero N seq}
+  add_mem' := by
+    intro a b
+    intro A B
+    simp at A
+    simp at B
+    simp
+    simp [sumZero] at A B
+    simp [sumZero]
+    simp [HAdd.hAdd]
+    simp [Add.add]
+    have eq: ∑ x, (a x + b x) = ∑ x, a x + ∑ x, b x := by
+      clear A B
+      simp [SeqFin] at a b
+      apply Finset.sum_add_distrib
+    rw [eq]
+    rw [A, B]
+    simp
+  zero_mem' := by
+    simp [sumZero]
+    simp [OfNat.ofNat]
+    simp [Zero.zero]
+  smul_mem' := by
+    intro c x
+    simp
+    intro CX
+    simp [sumZero] at CX
+    simp [sumZero]
+    simp [HSMul.hSMul]
+    simp [SMul.smul]
+    have eq: ∑ i, x i * c = c * ∑ i, x i := by
+      simp [Finset.mul_sum]
+      apply Finset.sum_congr
+      simp
+      intro i
+      intro I
+      generalize repl:x i = XI
+      simp [Int.mul_comm]
+    rw [eq]
+    rw [CX]
+    simp
+}
