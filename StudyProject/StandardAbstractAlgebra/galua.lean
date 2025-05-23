d38209870d37c42c1856dedabf5932de74f9ac41
BalandinIlia <ilia.balandin@student.uva.nl>
2025-05-23 19:45:15 +0200

    Update galua.lean

--- StudyProject/StandardAbstractAlgebra/galua.lean
+++ StudyProject/StandardAbstractAlgebra/galua.lean
@@ -123,24 +123,24 @@ macro "prove" x:ident X:ident solCase:tactic : tactic =>
 `(tactic|
 (
   apply sep $x $X;
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
-  $solCase
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
+  $solCase;
   $solCase
 )
 )
@@ -217,6 +217,7 @@ instance ins: AddCommMonoid Elem :=
     prove x eqx (prove y eqy (prove z eqz (
                                           intro e1 e2 e3
                                           simp [e1, e2, e3, Add.add]
+                                          clear e1 e2 e3
                                           )
                              )
                 )
@@ -247,7 +248,8 @@ instance ins: AddCommMonoid Elem :=
     simp [HAdd.hAdd]
     prove a.val a.prop (prove b.val b.prop (
                                            intro e1 e2
-                                           simp [e1, e2]                                           
+                                           simp [e1, e2]
+                                           clear e1 e2
                                            )                 
                        )
 
