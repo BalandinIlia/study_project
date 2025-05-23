25d84b59412ad1578dc6a9845b4abfd0deaa56b9
BalandinIlia <ilia.balandin@student.uva.nl>
2025-05-23 15:37:13 +0200

    Update galua.lean

--- StudyProject/StandardAbstractAlgebra/galua.lean
+++ StudyProject/StandardAbstractAlgebra/galua.lean
@@ -1,4 +1,5 @@
 import Mathlib.Data.Int.Basic
+import Mathlib.Data.Set.Basic
 import Mathlib.Algebra.Ring.Basic
 
 set_option maxHeartbeats 10000000
@@ -22,18 +23,11 @@ theorem test_ten_th: test_ten = {8,9,10,11,12,13,14,15,16,17} := by
   rw [test_ten]
   simp
 
-set_ten q 0
-set_ten w 10
-set_ten e 20
-set_ten r 30
-set_ten t 40
-set_ten y 50
-set_ten u 60
-set_ten i 70
-set_ten o 80
-set_ten p 90
-
-def universRaw: Set ℤ := q∪w∪e∪r∪t∪y∪u∪i∪o∪p
+def universRaw: Set ℤ :=
+{
+0,1,2,3,4,5,6,7,8,9,
+10,11,12,13,14,15,16,17,18,19
+}
 #check universRaw
 #print universRaw
 
@@ -58,391 +52,128 @@ lemma sep(a:ℤ)(belong:a∈universRaw)(A:Prop):
 (a=17 → A) →
 (a=18 → A) →
 (a=19 → A) →
-(a=20 → A) →
-(a=21 → A) →
-(a=22 → A) →
-(a=23 → A) →
-(a=24 → A) →
-(a=25 → A) →
-(a=26 → A) →
-(a=27 → A) →
-(a=28 → A) →
-(a=29 → A) →
-(a=30 → A) →
-(a=31 → A) →
-(a=32 → A) →
-(a=33 → A) →
-(a=34 → A) →
-(a=35 → A) →
-(a=36 → A) →
-(a=37 → A) →
-(a=38 → A) →
-(a=39 → A) →
-(a=40 → A) →
-(a=41 → A) →
-(a=42 → A) →
-(a=43 → A) →
-(a=44 → A) →
-(a=45 → A) →
-(a=46 → A) →
-(a=47 → A) →
-(a=48 → A) →
-(a=49 → A) →
-(a=50 → A) →
-(a=51 → A) →
-(a=52 → A) →
-(a=53 → A) →
-(a=54 → A) →
-(a=55 → A) →
-(a=56 → A) →
-(a=57 → A) →
-(a=58 → A) →
-(a=59 → A) →
-(a=60 → A) →
-(a=61 → A) →
-(a=62 → A) →
-(a=63 → A) →
-(a=64 → A) →
-(a=65 → A) →
-(a=66 → A) →
-(a=67 → A) →
-(a=68 → A) →
-(a=69 → A) →
-(a=70 → A) →
-(a=71 → A) →
-(a=72 → A) →
-(a=73 → A) →
-(a=74 → A) →
-(a=75 → A) →
-(a=76 → A) →
-(a=77 → A) →
-(a=78 → A) →
-(a=79 → A) →
-(a=80 → A) →
-(a=81 → A) →
-(a=82 → A) →
-(a=83 → A) →
-(a=84 → A) →
-(a=85 → A) →
-(a=86 → A) →
-(a=87 → A) →
-(a=88 → A) →
-(a=89 → A) →
-(a=90 → A) →
-(a=91 → A) →
-(a=92 → A) →
-(a=93 → A) →
-(a=94 → A) →
-(a=95 → A) →
-(a=96 → A) →
-(a=97 → A) →
-(a=98 → A) →
-(a=99 → A) →
 A := by
   rw [universRaw] at belong
-  rw [q,w,e,r,t,y,u,i,o,p] at belong
-  simp at belong
   cases belong
-  case inr belong => cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong => aesop
-  case inl belong =>
+  case inl => aesop
+  case inr belong =>
   cases belong
-  case inr belong => cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong => aesop
-  case inl belong =>
+  case inl => aesop
+  case inr belong =>
   cases belong
-  case inr belong => cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong => aesop
-  case inl belong =>
+  case inl => aesop
+  case inr belong =>
   cases belong
-  case inr belong => cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong => aesop
-  case inl belong =>
+  case inl => aesop
+  case inr belong =>
   cases belong
-  case inr belong => cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong => aesop
-  case inl belong =>
+  case inl => aesop
+  case inr belong =>
   cases belong
-  case inr belong => cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong => aesop
-  case inl belong =>
+  case inl => aesop
+  case inr belong =>
   cases belong
-  case inr belong => cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong => aesop
-  case inl belong =>
+  case inl => aesop
+  case inr belong =>
   cases belong
-  case inr belong => cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong => aesop
-  case inl belong =>
+  case inl => aesop
+  case inr belong =>
   cases belong
-  case inr belong => cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong => aesop
-  case inl belong =>
+  case inl => aesop
+  case inr belong =>
   cases belong
-  case inr belong => cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong =>
-                     cases belong
-                     case inl => aesop
-                     case inr belong => aesop
+  case inl => aesop
+  case inr belong =>
+  cases belong
+  case inl => aesop
+  case inr belong =>
+  cases belong
+  case inl => aesop
+  case inr belong =>
+  cases belong
+  case inl => aesop
+  case inr belong =>
+  cases belong
+  case inl => aesop
+  case inr belong =>
+  cases belong
+  case inl => aesop
+  case inr belong =>
+  cases belong
+  case inl => aesop
+  case inr belong =>
+  cases belong
+  case inl => aesop
+  case inr belong =>
+  cases belong
+  case inl => aesop
+  case inr belong =>
+  cases belong
+  case inl => aesop
+  case inr belong =>
+    simp at belong
+    aesop
 
 structure Elem where
 val: ℤ
 prop: val∈universRaw
 
+-- Solces goal like x∈inversRaw → Prop
+-- solCase solces the goal for each particular x value
+macro "prove" x:ident X:ident solCase:tactic : tactic =>
+`(tactic|
+(
+  apply sep $x $X
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+  $solCase
+)
+)
+
+axiom bel(z:ℤ):(z>-1)→(z<100)→z∈universRaw
+
 instance ins: AddCommMonoid Elem :=
 {
   add(e1 e2: Elem) :=
   {
-    val := (e1.val + e2.val)%100
+    val := (e1.val + e2.val)%20
     prop := by
-      sorry
+      generalize r1:e1.val = x
+      rw [Eq.comm] at r1
+      generalize r2:e2.val = y
+      rw [Eq.comm] at r2
+      have eq1:x∈universRaw:=by
+        simp [r1]
+        apply e1.prop
+      have eq2:y∈universRaw:=by
+        simp [r2]
+        apply e2.prop
+      clear r1 r2 e1 e2
+      prove x eq1 (prove y eq2 (
+                               intro e1 e2
+                               simp [e1, e2]
+                               apply bel
+                               omega
+                               omega
+                               )
+                  )
   }
   add_assoc := by
     sorry
